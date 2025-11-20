876b7699f1a20eabe944c8fc12ad850d36b3ad12
ngocnhi2911 <ngocyennhi2911@gmail.com>
2025-11-21 01:50:35 +0700

    bài tập tuần 7_BT3 + BT3 mở rộng: Assert

--- src/test/java/bt_webelement_webdriver/AddNewLeads.java
+++ src/test/java/bt_webelement_webdriver/AddNewLeads.java
@@ -1,10 +1,13 @@
 package bt_webelement_webdriver;
 
 import bt_locators.LocatorsLeadsCRM;
+import common.BaseTest;
 import login_crm.LoginCRM;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,7 +17,7 @@ import java.util.Random;
 import static bt_locators.LocatorsLeadsCRM.*;
 import static bt_locators.LocatorsLeadsCRM.getValueCountry;
 
-public class AddNewLeads extends LoginCRM {
+public class AddNewLeads extends BaseTest {
 
 //    public static void openNewLeadsPage() throws InterruptedException {
 //        driver.findElement(By.xpath(LocatorsLeadsCRM.menuLeads)).click();
@@ -35,7 +38,18 @@ public class AddNewLeads extends LoginCRM {
         //click menu Lead
         driver.findElement(By.xpath(menuLeads)).click();
         Thread.sleep(2000);
-        verifyDisplay(headerLeadPage, "Đã tới trang Leads", "FAILED!!! Không truy cập được vào trang Leads");
+        // Verify menu Lead
+        // Sau khi vào menu lead, check xem có hiển thị header Leads không?
+        driver.findElement(By.xpath(iconLeadsSummary)).click();
+        Thread.sleep(1000);
+
+        List<WebElement> checkHeaderLeadPage= driver.findElements(By.xpath(headerLeadPage));
+        System.out.println("checkHeaderLeadPage: " + checkHeaderLeadPage.size());
+        Assert.assertTrue(checkHeaderLeadPage.size() > 0, "Không truy cập được vào trang Leads!");
+
+        // So sánh text header lead có đúng với kết quả mk mong muốn hay k
+        String headerLeadsText = driver.findElement(By.xpath(headerLeadPage)).getText();
+        softAssert.assertEquals(headerLeadsText, "Leads Summary", "Header Leads is not correct");
 
     }
 
@@ -43,7 +57,15 @@ public class AddNewLeads extends LoginCRM {
         //click button New Lead
         driver.findElement(By.xpath(buttonNewLead)).click();
         Thread.sleep(1000);
-        verifyDisplay(headerAddNewLead, "Mở pop-up Add new lead thành công", "FAILED!!! Không mở được pop-up Add new lead");
+     //   verifyDisplay(headerAddNewLead, "Mở pop-up Add new lead thành công", "FAILED!!! Không mở được pop-up Add new lead");
+
+        List<WebElement> checkHeaderAddNewLeadPage= driver.findElements(By.xpath(headerAddNewLead));
+        System.out.println("checkHeaderAddNewLeadPage: " + checkHeaderAddNewLeadPage.size());
+        Assert.assertTrue(checkHeaderAddNewLeadPage.size() > 0, "Không mở được popup Add new Lead !");
+
+        // So sánh header có đúng với kết quả mk mong muốn hay k
+        String headerAddNewLeadsText = driver.findElement(By.xpath(headerAddNewLead)).getText();
+        softAssert.assertEquals(headerAddNewLeadsText, "Add new lead", "Header Add new lead is not correct");
 
     }
 
@@ -102,24 +124,17 @@ public class AddNewLeads extends LoginCRM {
         driver.findElement(By.xpath(LocatorsLeadsCRM.inputName)).sendKeys(name);
         Thread.sleep(1000);
 
-
         driver.findElement(By.xpath(LocatorsLeadsCRM.inputAddress)).sendKeys(address);
         Thread.sleep(1000);
-
 
         driver.findElement(By.xpath(LocatorsLeadsCRM.inputPosition)).sendKeys(position);
         Thread.sleep(1000);
 
-
         driver.findElement(By.xpath(LocatorsLeadsCRM.inputCity)).sendKeys(city);
         Thread.sleep(1000);
 
-//        driver.findElement(By.xpath(LocatorsLeadsCRM.inputEmailAddress)).sendKeys("ngocyennhi2911@gmail.com");
-//        Thread.sleep(1000);
-
         driver.findElement(By.xpath(LocatorsLeadsCRM.inputEmailAddress)).sendKeys(emailAddress);
         Thread.sleep(1000);
-
 
         driver.findElement(By.xpath(LocatorsLeadsCRM.inputState)).sendKeys(state);
         Thread.sleep(1000);
@@ -182,8 +197,6 @@ public class AddNewLeads extends LoginCRM {
         }
         Thread.sleep(1000);
 
-//        driver.findElement(By.xpath(LocatorsLeadsCRM.inputDateContacted)).sendKeys("08-11-2025 06:00");
-//        Thread.sleep(1000);
 
 //        // B1: Lấy thời gian hiện tại
 //        LocalDateTime now = LocalDateTime.now();
@@ -198,7 +211,38 @@ public class AddNewLeads extends LoginCRM {
 
         driver.findElement(By.xpath(LocatorsLeadsCRM.buttonSave)).click();
         Thread.sleep(4000);
+
+        // Check sau khi save có lỗi required không
+       List<WebElement> errorsRequired = driver.findElements(By.xpath(alertErrorMessageRequired));
+
+        //  Nếu tìm thấy lỗi (>0) thì lấy text ra in vào log
+        if (errorsRequired.size() > 0) {
+            String errorMsg = "";
+            // Duyệt qua tất cả các lỗi tìm thấy
+            for (WebElement err : errorsRequired) {
+                errorMsg = errorMsg + err.getText();
+            }
+            Assert.fail("FAILED: Save không thành công! Nội dung lỗi: " + errorMsg);
+        }
+        Thread.sleep(3000);
     }
+
+    public static void verifyEmailResult(boolean expectValid, String expectedErrorMsg) {
+        List<WebElement> checkErrorEmail = driver.findElements(By.xpath(alertErrorMessageEmail));
+        System.out.println("checkErrorEmail.size(): " + checkErrorEmail.size());
+
+        if (expectValid) {
+            Assert.assertEquals(checkErrorEmail.size(), 0, "Không thể thêm mới vì Email lỗi !!! Vui lòng kiểm tra lại");
+        } else {
+            // Case: email SAI / TRÙNG → PHẢI có lỗi
+            Assert.assertTrue(checkErrorEmail.size() > 0, "Nhập email sai/trùng nhưng không hiển thị lỗi Email !!!");
+
+            String actualMsg = checkErrorEmail.get(0).getText().trim();
+            Assert.assertTrue(actualMsg.contains(expectedErrorMsg),
+                    "Thông báo lỗi email không đúng. Expected: " + expectedErrorMsg + " | Actual: " + actualMsg);
+        }
+    }
+
 
     public static void searchLeads(String leadsName) throws InterruptedException {
         driver.findElement(By.xpath(LocatorsLeadsCRM.iconCloseProfile)).click();
@@ -207,31 +251,24 @@ public class AddNewLeads extends LoginCRM {
         driver.findElement(By.xpath(LocatorsLeadsCRM.inputSearch)).sendKeys(leadsName);
         Thread.sleep(2000);
 
-        String firstRowLeads = driver.findElement(By.xpath(LocatorsLeadsCRM.firstRowItemLeads)).getText();
-        System.out.println("First row leads: " + firstRowLeads);
+        // Check xem có tìm thấy dòng nào không
+        List<WebElement> rows = driver.findElements(By.xpath(firstRowItemLeads));
+        Assert.assertTrue(rows.size() > 0, "FAILED: Không tìm thấy Lead vừa tạo trong danh sách!");
+
+        System.out.println("Đã tìm thấy Lead search: " + rows.get(0).getText());
         Thread.sleep(2000);
 
     }
 
-    //Hàm so sánh giá trị đã thêm mới trong màn edit
-    public static void compareFieldAttribute(String expectedValue, String xpathActual, String attributeActual) {
+    public static void compareFieldAttribute(String expectedValue, String xpathActual, String attributeType) {
         String expected = expectedValue;
-        String actual = driver.findElement(By.xpath(xpathActual)).getAttribute(attributeActual);
-        if (actual.trim().equalsIgnoreCase(expected.trim())) {
-            System.out.println("Giá trị hiển thị đúng: " + actual);
-        } else {
-            System.out.println("FAIL: Giá trị mong muốn là: " + expected + " nhưng giá trị thực tế là: " + actual);
-        }
+        String actual = driver.findElement(By.xpath(xpathActual)).getAttribute(attributeType);
+        softAssert.assertEquals(actual, expectedValue, "FAIL: Giá trị mong muốn là: " + expected + " nhưng giá trị thực tế là: " + actual);
     }
 
     public static void verifyCheckboxSelected(String checkbox) {
         boolean checked = driver.findElement(By.xpath(checkbox)).isSelected();
-        if (checked) {
-            System.out.println("Checkbox is selected: " + checked);
-        } else {
-            System.out.println("Checkbox is selected: " + checked);
-
-        }
+        Assert.assertTrue(checked, "FAILED: Checkbox [" + checked + "] chưa được chọn.");
     }
 
 
@@ -250,6 +287,18 @@ public class AddNewLeads extends LoginCRM {
 
         driver.findElement(By.xpath(linkEdit)).click();
         Thread.sleep(3000);
+
+
+        // verify đã vào được màn Edit hay chưa
+        List<WebElement> profileTabs = driver.findElements(By.xpath(tabProfile));
+        Assert.assertTrue(profileTabs.size() > 0, "FAILED: Không tìm thấy tab Profile, chưa vào được màn hình Edit Lead.");
+
+
+        //Verify có vào đúng màn Edit có Tiêu đề edit chứa tên Lead mong muốn theo name
+        String actualTitledit = driver.findElement(By.xpath(headerEditPage)).getText();
+        System.out.println("Tiêu đề popup: " + actualTitledit);
+        Assert.assertTrue(actualTitledit.contains(name), "FAILED: Tiêu đề popup không chứa tên Lead! Mở sai pop-up Lead edit hoặc chưa mở được popup edit.");
+
 
         compareFieldAttribute(status, dropdownStatus, "title");
         compareFieldAttribute(source, dropdownSource, "title");
@@ -273,20 +322,39 @@ public class AddNewLeads extends LoginCRM {
         verifyCheckboxSelected(checkboxPublic);
     }
 
-//    private boolean compareField(String fieldName, String expected, String actual) {
-//        if (expected == null) expected = "";
-//        if (actual == null) actual = "";
-//
-//        if (!expected.equals(actual)) {
-//            System.out.println("❌" + fieldName + " không khớp. Expected: [" + expected + "] | Actual: [" + actual + "]");
-//            return false;
-//        } else {
-//            System.out.println("✅" + fieldName + " khớp");
-//            return true;
-//        }
-//    }
 
-    @Test
+    public static void deleteLead(String leadsName) throws InterruptedException {
+        WebElement firstRow = driver.findElement(By.xpath(LocatorsLeadsCRM.firstRow));
+        // B2: Hover chuột vào dòng đầu tiên
+        Actions actions = new Actions(driver);
+        actions.moveToElement(firstRow).perform();
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath(linkDelete)).click();
+        Thread.sleep(3000);
+
+        //Khởi tạo class Alert
+        Alert alert1 = driver.switchTo().alert();
+        //Dùng hàm accept() để xác nhận Alert (tương ứng click vào nút OK)
+        alert1.accept(); // chấm hàm để sử dụng
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath(LocatorsLeadsCRM.inputSearch)).clear();
+        driver.findElement(By.xpath(LocatorsLeadsCRM.inputSearch)).sendKeys(leadsName);
+        Thread.sleep(2000);
+
+        List<WebElement> actualResults = driver.findElements(By.xpath(firstRowItemLeads));
+        Assert.assertEquals(actualResults.size(), 0, "FAILED: Lead vẫn tìm thấy sau khi xóa. Số lượng kết quả: " + actualResults.size());
+
+        System.out.println("Không tìm thấy Lead '" + leadsName + "' trong kết quả tìm kiếm.");
+
+
+    }
+
+
+
+    @Test(priority = 1)
     public void testAddAndVerifyLead() throws InterruptedException{
         //openNewLeadsPage();
 
@@ -296,11 +364,11 @@ public class AddNewLeads extends LoginCRM {
         String source = "Google";
         String assigned = "Admin Anh Tester";
         String tag = "JSC_NEW";
-        String name = "Yến Nhi 7";
+        String name = "Yến Nhi 1";
         String address = "Đại Linh";
         String position = "Tester";
         String city = "Việt Nam";
-        String emailAddress = "ngocnhi7@gmail.com";
+        String emailAddress = "ngocnhi1@gmail.com";
         String state = "Hà Nội";
         String website = "htester.com.vn";
         String country = "Vietnam";
@@ -323,25 +391,25 @@ public class AddNewLeads extends LoginCRM {
                 company, description, dateContacted);
        Thread.sleep(2000);
 
-        searchLeads("Yến Nhi 7");
+        verifyEmailResult(true, "");
+
+
+        searchLeads("Yến Nhi 1");
         Thread.sleep(2000);
+
     }
 
-    @Test
+    @Test(priority = 2)
     public void testEditLead() throws InterruptedException{
-        //openNewLeadsPage();
-
-        //  AddNewLeads addLead = new AddNewLeads();
-
         String status = "Active";
         String source = "Google";
         String assigned = "Admin Anh Tester";
         String tag = "JSC_NEW";
-        String name = "Yến Nhi 10";
+        String name = "Yến Nhi 2";
         String address = "Đại Linh";
         String position = "Tester";
         String city = "Việt Nam";
-        String emailAddress = "ngocnhi10@gmail.com";
+        String emailAddress = "ngocnhi2@gmail.com";
         String state = "Hà Nội";
         String website = "htester.com.vn";
         String country = "Vietnam";
@@ -364,7 +432,10 @@ public class AddNewLeads extends LoginCRM {
                 company, description, dateContacted);
         Thread.sleep(2000);
 
-        searchLeads("Yến Nhi 10");
+        verifyEmailResult(true, "");
+        Thread.sleep(2000);
+
+        searchLeads("Yến Nhi 2");
         Thread.sleep(2000);
 
         editLead(status, source, assigned, tag, name, address, position, city,
@@ -373,5 +444,51 @@ public class AddNewLeads extends LoginCRM {
         Thread.sleep(2000);
 
     }
+
+    @Test(priority = 3)
+    public void testDeleteLead() throws InterruptedException{
+        //openNewLeadsPage();
+
+        //  AddNewLeads addLead = new AddNewLeads();
+
+        String status = "Active";
+        String source = "Google";
+        String assigned = "Admin Anh Tester";
+        String tag = "JSC_NEW";
+        String name = "Yến Nhi 3";
+        String address = "Đại Linh";
+        String position = "Tester";
+        String city = "Việt Nam";
+        String emailAddress = "ngocnhi3@gmail.com";
+        String state = "Hà Nội";
+        String website = "htester.com.vn";
+        String country = "Vietnam";
+        String phone = "0965898989";
+        String zipCode = "0001";
+        String leadValue = "12345";
+        String language = "Vietnamese";
+        String company = "NODO JSC";
+        String description = "htest add new lead";
+        String dateContacted = "10-11-2025 00:00:00";
+
+        //click menu Lead
+        verifyMenuLead();
+
+        //click btn addnewLead
+        verifyBtnAddNewLead();
+
+        addNewLeads(status, source, assigned, tag, name, address, position, city,
+                emailAddress, state, website, country, phone, zipCode, leadValue, language,
+                company, description, dateContacted);
+        Thread.sleep(2000);
+
+        searchLeads("Yến Nhi 3");
+        Thread.sleep(2000);
+
+        deleteLead("Yến Nhi 3");
+        Thread.sleep(2000);
+
+    }
+
 
 }
---
+++ src/test/java/common/BaseTest.java
@@ -0,0 +1,67 @@
+package common;
+
+import bt_locators.LocatorsLeadsCRM;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+import java.time.Duration;
+import java.util.List;
+
+public class BaseTest {
+    public static WebDriver driver;
+    public static SoftAssert softAssert;
+
+    @BeforeMethod
+    public static void createDriver() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        softAssert = new SoftAssert();
+        System.out.println("Create Chrome driver");
+        loginCRM();
+    }
+
+    public static void loginCRM() throws InterruptedException {
+        driver.get(LocatorsLeadsCRM.url);
+
+        // Verify hiển thị header 'Login'
+        List<WebElement> checkHeaderLogin = driver.findElements(By.xpath(LocatorsLeadsCRM.headerLoginPage));
+        System.out.println("checkHeaderLogin: " + checkHeaderLogin.size());
+        Assert.assertTrue(checkHeaderLogin.size() > 0, "Header Login is not displayed");
+
+        // So sánh header Login có đúng với kết quả mk mong muốn hay k
+        String headerLoginText = driver.findElement(By.xpath(LocatorsLeadsCRM.headerLoginPage)).getText();
+        Assert.assertEquals(headerLoginText, "Login", "Header Login is not correct");
+
+
+        driver.findElement(By.xpath(LocatorsLeadsCRM.inputEmail)).clear();
+        driver.findElement(By.xpath(LocatorsLeadsCRM.inputEmail)).sendKeys("admin@example.com");
+        driver.findElement(By.xpath(LocatorsLeadsCRM.inputPassword)).clear();
+        driver.findElement(By.xpath(LocatorsLeadsCRM.inputPassword)).sendKeys("123456");
+        driver.findElement(By.xpath(LocatorsLeadsCRM.buttonLogin)).click();
+        Thread.sleep(2000);
+
+
+        // Sau khi đăng nhập thành công --> hiển thị menu Dashboard
+        // Verify menu Dashboard
+        List<WebElement> checkMenuDashboard = driver.findElements(By.xpath(LocatorsLeadsCRM.menuDashboard));
+        System.out.println("checkMenuDashboard: " + checkMenuDashboard.size());
+        Assert.assertTrue(checkMenuDashboard.size() > 0, "Menu Dashboard is not displayed after login");
+
+    }
+
+    @AfterMethod
+    public static void closeDriver(){
+        if(driver != null){
+            driver.quit();
+            softAssert.assertAll();
+            System.out.println("Closed Chrome driver");
+        }
+    }
+}
